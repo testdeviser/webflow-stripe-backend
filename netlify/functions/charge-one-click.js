6fffc6e61d8fff4af9225a39797d8b4541a1f0f5
testdeviser <testdeviser@gmail.com>
2025-04-25 12:25:41 +0530

    Update Changes to this file

--- netlify/functions/charge-one-click.js
+++ netlify/functions/charge-one-click.js
@@ -1,5 +1,15 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+const DOWNLOAD_LINKS = {
+  "Ultimate Organic Package": "https://www.notion.so/Ultimate-Organic-Post-Swipe-File-1c02b1d91a02802ea91cd44745b72e6c?pvs=4",
+  "Ultimate Ads Package": "https://www.notion.so/Ultimate-Ads-Package-Swipe-File-1c02b1d91a0280f78300d81843a4cb77?pvs=4",
+  "Ultimate Brand Scaling Package": "https://nebula-bard-3b2.notion.site/Ultimate-Brand-Scaling-Static-Ad-Swipe-File-1bd2b1d91a028189966cd5d6d9d98643?pvs=4",
+  "THE 7-FIGURE META ADS PLAYBOOK": "https://nebula-bard-3b2.notion.site/The-7-Figure-Meta-Ads-Playbook-1bd2b1d91a0281c2a754e0c20034cfd3?pvs=4",
+  //"5 DFY STATIC ADS DONE FOR YOU": "https://yourdomain.com/downloads/5-dfy-ads.zip",
+  //"10 DFY STATIC ADS DONE FOR YOU": "https://yourdomain.com/downloads/10-dfy-ads.zip",
+};
+
+
 exports.handler = async (event) => {
   if (event.httpMethod === "OPTIONS") {
     return {
@@ -31,7 +41,7 @@ exports.handler = async (event) => {
     // Create payment intent
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-	  name: customer_name,
+	    name: customer_name,
       currency: "usd",
       customer: customer_id,
       payment_method: defaultPaymentMethod,
@@ -39,12 +49,39 @@ exports.handler = async (event) => {
       confirm: true,
       metadata: {
         product_name: product_name || "Upsell Product",
-		email : customer_email,
-		phone:customer_phone,
+		    email : customer_email,
+		    phone:customer_phone,
         type: "upsell_product",
       },
     });
+    
+    try {
+      const ghlWebhookPayload = {
+        name: pi.metadata.customer_name || 'Customer',
+        email: pi.receipt_email || pi.metadata.email || "unknown",
+        phone:pi.metadata.phone,
+        product: pi.metadata.product_name,
+        amount: pi.amount / 100,
+        type: pi.metadata.type,
+       // download_url: pi.metadata.download_url, // Make sure this is passed in Stripe metadata
+          date: new Date(pi.created * 1000).toISOString(),
+          download_url: DOWNLOAD_LINKS[pi.metadata.product_name],
+      };
 
+      const ghlRes = await fetch(process.env.GHL_WEBHOOK_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(ghlWebhookPayload)
+      });
+
+      const ghlResult = await ghlRes.text();
+      console.log("✅ Sent data to GHL:", ghlResult);
+      console.log(DOWNLOAD_LINKS[pi.metadata.product_name]);
+      } catch (err) {
+        console.error("❌ Failed to send data to GHL:", err.message);
+      }
     // Determine redirect flow
     let redirect_url = "/thank-you";
     if (amount === 2700) redirect_url = "/upsell-2?customer_id=" + customer_id;
